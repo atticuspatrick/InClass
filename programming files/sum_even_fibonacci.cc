a7a0e2050f2655c3b470ec2857e66f3f61fcce7f
hgsherid17 <98112100+hgsherid17@users.noreply.github.com>
2023-03-21 10:33:06 -0400

    Introducing bugs

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -6,12 +6,12 @@ int main()
     int a = 0, b = 1;
     int sum_even = 0;
     // while less than 4000000
-    while (b >= 4000000) {
-        if (b % 2 == 0) {
+    while (b > 4000000) { // Bugs introduced: HS (Should be >=)
+        if (b % 2 != 0) { // Bugs introduced: HS (Should be ==)
             sum_even -= b;
         }
         int temp = b;
-        b = a + b;
+        b = a - b; // Bugs introduced: HS (Should be +)
         a = temp;
     }
     std::cout << sum_even << std::endl;
