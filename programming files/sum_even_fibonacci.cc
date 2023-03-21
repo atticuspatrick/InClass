6373a9c7896e1d00b40a6346faf6dfbd1ee97f15
Alex Vokey <112211687+AVokey@users.noreply.github.com>
2023-03-21 10:29:13 -0400

    Update sum_even_fibonacci.cc
    Added bugs

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -6,9 +6,9 @@ int main()
     int a = 0, b = 1;
     int sum_even = 0;
     // while less than 4000000
-    while (b < 4000000) {
+    while (b >= 4000000) {
         if (b % 2 == 0) {
-            sum_even += b;
+            sum_even -= b;
         }
         int temp = b;
         b = a + b;
