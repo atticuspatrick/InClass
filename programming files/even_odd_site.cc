835eb1a99e565f8ca2d86cf251c4427ab762e5b6
Atticus Patrick <68357930+atticuspatrick@users.noreply.github.com>
2023-03-21 10:30:47 -0400

    Update even_odd_site.cc

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -1,3 +1,5 @@
+#bugs introduced AP
+
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -10,7 +12,7 @@ int main() {
     file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
     // Display table
     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
-    for (int i = 1; i <= 50; i++) {
+    for (int i = 1; i <= -50; i++) {
         if (i % 2 == 0) {
             // Add even numbers to table
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
