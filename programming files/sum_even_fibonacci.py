c5a6fd263193209de476ddc00d112e30081b4e8b
anthonymazzola <112509417+anthonymazzola@users.noreply.github.com>
2023-03-21 10:30:04 -0400

    Added bugs

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,14 +1,14 @@
 #bugs introduced AP
 
 #initialize a, b
-a, b = 0, 1
+a b = 0, 1
 sum_even = 0
 #while loop for less than 4000000
-while c < 4000000:
+while b > 4000000:
     if b % 2 == 0:
         sum_even += b
     a, b = b, a+b
 #print result
-print(sum_even)
+print(sumEven)
 
 # answer should be 4613732
