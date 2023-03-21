cddbea47a923b49d9dc428d6c9baff3904834123
Atticus Patrick <68357930+atticuspatrick@users.noreply.github.com>
2023-03-21 10:28:54 -0400

    Update sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,8 +1,10 @@
+#bugs introduced AP
+
 #initialize a, b
 a, b = 0, 1
 sum_even = 0
 #while loop for less than 4000000
-while b < 4000000:
+while c < 4000000:
     if b % 2 == 0:
         sum_even += b
     a, b = b, a+b
